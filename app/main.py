193529c8d13cfeab6b0319ad2786470173025ebc
Bondarenko K <bku089@gmail.com>
2026-06-17 00:55:01 +0300

    fix flake8

--- app/main.py
+++ app/main.py
@@ -5,7 +5,9 @@ def move_file(command: str) -> None:
     parts = command.split()
 
     if len(parts) != 3 or parts[0] != "mv":
-        raise ValueError("Invalid command format. Expected: 'mv src_file dst_file'")
+        raise ValueError(
+            "Invalid command format. Expected: 'mv src_file dst_file'"
+        )
 
     command_name, src, dst = parts
 
@@ -15,11 +17,13 @@ def move_file(command: str) -> None:
     dir_path = os.path.dirname(dst)
 
     if dir_path:
-        sub_dirs = dir_path.split(os.sep) if os.sep in dir_path else dir_path.split("/")
+        sub_dirs = dir_path.split(os.sep)\
+            if os.sep in dir_path else dir_path.split("/")
         current_path = ""
         for folder in sub_dirs:
             if folder:
-                current_path = os.path.join(current_path, folder) if current_path else folder
+                current_path = os.path.join(current_path, folder)\
+                    if current_path else folder
                 if not os.path.exists(current_path):
                     os.mkdir(current_path)
 
